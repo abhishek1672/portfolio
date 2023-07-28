1b6e0fdc5a9b24dcf711a696d007d884ab0f03d0
Abhishek <abhinalawade167@gmail.com>
2023-07-28 21:20:44 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    var typed = new Typed(".multiple-text", {
+        strings: ["MCA Student.", "Developement Lover.", "Future Java Developer.", "Problem Solver."],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
+}, 4000);
