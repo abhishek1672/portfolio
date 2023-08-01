0594910ea8b80581b222ea7bb12f2d32c2eedb84
Abhishek <abhinalawade167@gmail.com>
2023-08-01 09:18:31 +0530

    navbar change

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ hamburger.addEventListener('click', () => {
 // Close the navbar when a navigation link is clicked
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        navbar.classList.remove('active');
+        if (window.innerWidth <= 767) { // Check if the screen width is smaller or equal to 767px (mobile screen)
+            navbar.classList.remove('active'); // Close the navbar on mobile screens
+        }
     });
 });
 
