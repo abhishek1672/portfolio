9b6c7de70f372ad008759be0ab4b6d4137911fba
Abhishek <abhinalawade167@gmail.com>
2023-08-01 13:04:40 +0530

    js change

--- script.js
+++ script.js
@@ -8,25 +8,6 @@ setTimeout(() => {
     })
 }, 4000);
 
-const hamburger = document.querySelector('.hamburger');
-const navbar = document.querySelector('.navbar');
-const navLinks = document.querySelectorAll('.navbar a');
-
-hamburger.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
-
-// Close the navbar when a navigation link is clicked
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        if (window.innerWidth <= 767) { // Check if the screen width is smaller or equal to 767px (mobile screen)
-            navbar.classList.remove('active'); // Close the navbar on mobile screens
-        }
-    });
-});
-
-
-
 
 
 
